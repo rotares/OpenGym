4fa2a22ce76b4d05ea07d41a5c0fab86ad40040d
Pavel Prigoda <rotares@disroot.org>
2026-04-26 16:28:53 +0300

    feat(dep): install tanstack devtools

--- src/app/providers/app-provider.tsx
+++ src/app/providers/app-provider.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { ThemeProvider } from "./theme-provider"
 
 interface RouterProviderProps {
@@ -11,7 +12,10 @@ const queryClient = new QueryClient()
 export const AppProvider = ({ children }: RouterProviderProps) => {
   return (
     <ThemeProvider>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ThemeProvider>
   )
 }
