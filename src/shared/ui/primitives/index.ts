72817d53dc28f41ba5f1a4d1e11c6c06af0d1ce6
Pavel Prigoda <rotares@disroot.org>
2026-04-29 18:10:58 +0300

    feat(components): add new shadcn comp

--- src/shared/ui/primitives/index.ts
+++ src/shared/ui/primitives/index.ts
@@ -1,6 +1,8 @@
 // UI Kit - Public API
 // All UI components are exported here for use across the app
 
+export * from "./accordion"
+export { AspectRatio } from "./aspect-ratio"
 export { Button, buttonVariants } from "./button"
 export * from "./button-group"
 export { Input } from "./input"
