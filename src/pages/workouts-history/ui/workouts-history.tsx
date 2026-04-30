6ab4ffb2111234bb2b869fa688638763e8b8ccf4
Pavel Prigoda <rotares@disroot.org>
2026-04-30 17:06:52 +0300

    refactor(header store): delete header store

--- src/pages/workouts-history/ui/workouts-history.tsx
+++ src/pages/workouts-history/ui/workouts-history.tsx
@@ -1,21 +1,4 @@
-import { useHeaderStore } from "@/shared/store/header"
-import { Button } from "@/shared/ui/primitives"
-import { useEffect } from "react"
-
 export const WorkoutsHistoryPage = () => {
-  const setHeaderConfig = useHeaderStore((state) => state.setHeaderConfig)
-
-  //set header config for page
-  useEffect(() => {
-    setHeaderConfig({
-      actions: () => (
-        <Button onClick={() => alert("add workout")}>Add workout</Button>
-      ),
-    })
-
-    return () => setHeaderConfig(null)
-  }, [setHeaderConfig])
-
   return (
     <>
       <div>workouts</div>
