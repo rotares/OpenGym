c153f30eb36ebd2334ce7b6bae5efbfa7ed26971
Pavel Prigoda <rotares@disroot.org>
2026-05-03 15:58:23 +0300

    refactor(app-header): change app header

--- src/shared/ui/components/app-header/index.tsx
+++ src/shared/ui/components/app-header/index.tsx
@@ -1,12 +1,28 @@
+import { supabase } from "@/shared/api/supabaseClient"
+import { useAuth } from "@/shared/store/auth/useAuth"
+import { useNavigate } from "react-router"
+import { Button } from "../../primitives"
+
 //global header
 export const AppHeader = ({ actions, headerConfig }) => {
-  console.log("пропы app header", actions, headerConfig)
+  const { isAuth } = useAuth()
+  const navigate = useNavigate()
 
   return (
     <header className="bg-primary text-primary-foreground flex items-center px-4 py-3">
       <div className="flex gap-3 items-center justify-between @md:max-w-250 h-[35px]  mx-auto w-full">
-        <h1 className="text-xl ">OpenGym</h1>
+        <h1 onClick={() => navigate("/")} className="text-xl cursor-pointer">
+          OpenGym
+        </h1>
         {actions && <div>{actions}</div>}
+
+        {!isAuth && <Button onClick={() => navigate("/auth")}>Start</Button>}
+        {isAuth && (
+          <Button
+            onClick={async () => supabase.auth.signOut()}
+            children={"Logout"}
+          />
+        )}
       </div>
     </header>
   )
