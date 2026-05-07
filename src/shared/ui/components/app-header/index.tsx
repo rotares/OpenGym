dc0e10ff89eca59c8bfc0d362401b6ae0105cf34
Pavel Prigoda <rotares@disroot.org>
2026-05-07 05:25:45 +0300

    refactor(app header): change header

--- src/shared/ui/components/app-header/index.tsx
+++ src/shared/ui/components/app-header/index.tsx
@@ -1,13 +1,14 @@
 import { supabase } from "@/shared/api/supabaseClient"
 import { Button } from "@/shared/ui/primitives"
 import { useNavigate } from "react-router"
+
 //global header
 export const AppHeader = () => {
   const navigate = useNavigate()
 
   return (
-    <header className="bg-primary text-primary-foreground flex items-center px-4 py-3">
-      <div className="flex gap-3 items-center justify-between @md:max-w-250 h-[35px]  mx-auto w-full">
+    <header className="flex bg-primary text-primary-foreground items-center px-6 lg:px-10 py-3">
+      <div className="flex gap-3 items-center mx-auto w-full">
         <h1 onClick={() => navigate("/")} className="text-xl cursor-pointer">
           OpenGym
         </h1>
