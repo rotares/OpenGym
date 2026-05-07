d7750452df3a3a181c05f716edfaf4672b059936
Pavel Prigoda <rotares@disroot.org>
2026-05-07 05:24:27 +0300

    (sidebar, shadcn): add custom test sidebar

---
+++ src/widgets/layouts/sidebar/appSidebar.tsx
@@ -0,0 +1,29 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/shared/ui/primitives"
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton children={"button"} />
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
+        <SidebarGroup />
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  )
+}
