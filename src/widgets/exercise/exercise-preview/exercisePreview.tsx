771831234269d5c865c5a6d21a7f2688b84a37b8
Pavel Prigoda <rotares@disroot.org>
2026-05-09 19:53:13 +0300

    refactor(exercise page): change css styles

--- src/widgets/exercise/exercise-preview/exercisePreview.tsx
+++ src/widgets/exercise/exercise-preview/exercisePreview.tsx
@@ -2,8 +2,8 @@ import { AspectRatio } from "@/shared/ui/primitives/"
 
 export const ExercisePreview = () => {
   return (
-    <AspectRatio ratio={16 / 9} className="rounded-lg bg-muted">
-      <div className="h-[20vh] w-full"></div>
+    <AspectRatio ratio={16 / 9} className="rounded-lg bg-muted max-w[500px]">
+      <div className="h-[10vh]"></div>
     </AspectRatio>
   )
 }
