80343c2fe9a46318aade4dae7a5ca183a387d3c3
Pavel Prigoda <rotares@disroot.org>
2026-05-20 18:25:49 +0300

    refactor(nav-factory, sidebar): change logic onclick of close sidebar in mobile ver

--- src/widgets/sidebar/ui/nav-factory.tsx
+++ src/widgets/sidebar/ui/nav-factory.tsx
@@ -3,18 +3,21 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  useSidebar,
 } from "@/shared/ui/primitives"
 import { NavLink } from "react-router"
 import { type NavProps } from "../types"
 
 export const FactoryNav = ({ items, ...props }: NavProps) => {
+  const { setOpenMobile } = useSidebar()
+
   return (
     <SidebarGroup {...props}>
       <SidebarMenu className="gap-3">
         {items.map(({ label, name, route, icon: Icon }) => (
           <SidebarMenuItem className="justify-center" key={label}>
             <SidebarMenuButton asChild>
-              <NavLink to={route}>
+              <NavLink onClick={() => setOpenMobile(false)} to={route}>
                 {Icon && <Icon />}
                 <span>{name}</span>
               </NavLink>
