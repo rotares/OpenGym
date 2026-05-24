2b05e3f6da93c4532e1e464acdd73f1ba38c0562
Pavel Prigoda <rotares@disroot.org>
2026-05-24 10:44:42 +0300

    refactor(layout): change styles, add mark for drawer portal

--- src/widgets/layouts/main-layout/privateLayout.tsx
+++ src/widgets/layouts/main-layout/privateLayout.tsx
@@ -5,12 +5,43 @@ import { PrivateHeader } from "../header"
 //main app layout
 export const PrivateLayout = () => {
   return (
-    <SidebarProvider className="flex flex-col">
+    <SidebarProvider
+      className="
+        h-svh
+
+        flex
+        flex-col
+      "
+    >
       <PrivateHeader />
-      <div className="flex flex-1">
+
+      <div className="flex flex-1 overflow-hidden">
         <AppSidebar />
-        <SidebarInset className=" eflex-1 flex">
-          <div className="relative flex-1 flex flex-col w-full max-w-[980px] mx-auto p-6 lg:p-10">
+
+        <SidebarInset className="flex flex-1">
+          <div
+            className="
+              main-content-area
+              relative
+
+              flex
+              flex-1
+              flex-col
+
+              w-full
+              max-w-[980px]
+
+              mx-auto
+
+              overflow-hidden
+
+              px-6
+              pt-6
+
+              lg:px-10
+              lg:pt-10
+            "
+          >
             <Outlet />
           </div>
         </SidebarInset>
