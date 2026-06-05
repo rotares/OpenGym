298b2ceb44dd6b74e5d6e43c17744cd8354c1526
Pavel Prigoda <rotares@disroot.org>
2026-06-05 19:31:42 +0300

    feat(components): add page wrapper compound component

--- src/shared/ui/components/index.ts
+++ src/shared/ui/components/index.ts
@@ -1,5 +1,6 @@
 export { BackButton } from "./buttons/BackButton";
 export { LazyPage } from './lazy-page/LazyPage';
+export { PageWrapper } from './page-wrapper/PageWrapper';
 export { SearchInput } from './search-input/SearchInput';
 export { CustomSpinner } from "./spinner";
 
---
+++ src/shared/ui/components/page-wrapper/PageWrapper.tsx
@@ -0,0 +1,42 @@
+import { cn } from "@/shared/lib"
+import { type FC } from "react"
+
+type Props = {
+  children: React.ReactNode
+  className?: string
+  isCustom?: boolean
+}
+
+interface WrapperComponent extends FC<Props> {
+  Header: FC<Props>
+  Content: FC<Props>
+}
+
+export const PageWrapper: WrapperComponent = ({ children, className }) => {
+  return (
+    <section
+      className={cn(
+        className,
+        "flex flex-col gap-4 flex-1 w-full h-full no-scrollbar max-w-[1024px] mx-auto",
+      )}
+    >
+      {children}
+    </section>
+  )
+}
+
+PageWrapper.Header = ({ children, className, isCustom }) => {
+  return (
+    <header className={cn(!isCustom && "pb-2", className)}>{children}</header>
+  )
+}
+
+PageWrapper.Content = ({ children, className, isCustom }) => {
+  return (
+    <main
+      className={cn(!isCustom && "no-scrollbar overflow-y-auto", className)}
+    >
+      {children}
+    </main>
+  )
+}
