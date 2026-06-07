bd8c337e1f14602c60a00291866c6b8b7845d1aa
Pavel Prigoda <rotares@disroot.org>
2026-06-07 12:48:39 +0300

    refactor(workout): add new types, query config
    - Add types from api using supabase QueryData
    - Add DTO for List item, Workout details
    - Add query select string to config

---
+++ src/entities/workout/config/query-select.ts
@@ -0,0 +1,48 @@
+import { supabase } from "@/shared/api"
+
+const WORKOUT_DETAILS_SELECT = `
+    id,
+    title,
+    finished_at,
+    duration_minutes,
+    total_volume,
+    total_sets,
+    workout_exercises (
+      total_sets,
+      total_volume,
+      exercises (
+        name
+      ),
+      sets (
+        weight,
+        reps,
+        order_index
+      )
+    )
+`
+
+const workoutDetailsQuery = supabase.from('workouts').select(WORKOUT_DETAILS_SELECT).single()
+
+
+const WORKOUT_LIST_ITEM = 
+  ` 
+    title,
+    duration_minutes,
+    finished_at,
+    title,
+    total_sets,
+    total_volume,
+    workout_exercises (
+      total_sets,
+      exercises (
+        name
+      )
+    )
+  `
+
+const workoutListItemQuery = supabase.from('workouts').select(WORKOUT_LIST_ITEM)
+
+export {
+  WORKOUT_DETAILS_SELECT, WORKOUT_LIST_ITEM, workoutDetailsQuery, workoutListItemQuery
+}
+
--- src/entities/workout/model/types.ts
+++ src/entities/workout/model/types.ts
@@ -1,17 +1,63 @@
 // entities/workout/model/workout-store.types.ts
-
 import type { Tables } from "@/shared/api"
+import { type QueryData } from "@supabase/supabase-js"
+import { workoutDetailsQuery, workoutListItemQuery } from "../config/query-select"
 import type {
   WorkoutExercise,
   WorkoutSession,
   WorkoutSet,
 } from "./workout-session.types"
 
+//basic types
 export type Exercise = Tables<"exercises"> 
 export type Workout = Tables<"workouts">
 export type WorkoutExerciseType = Tables<"workout_exercises">
 
+//modified
+export type WorkoutSaving = Omit<Workout, 'created_at' | 'id'>
+export type ExerciseSaving = Omit<WorkoutExerciseType, 'id'>
 
+//raw data from api
+export type RawWorkoutDetails = QueryData<typeof workoutDetailsQuery>
+export type RawWorkoutListItem = QueryData<typeof workoutListItemQuery>
+
+//types for client
+export type ExercisePreview = {
+  name: string,
+  setsCount: number
+}
+
+export type WorkoutListItem = {
+    title: string,
+		date: string  
+		durationMinutes: number  
+		totalVolume: number  
+		totalSets: number  
+} & {
+  exercisesPreview: ExercisePreview[]
+}
+
+export type WorkoutDetails = {
+	id: string  
+	date: string  
+	durationMinutes: number  
+	totalVolume: number  
+	totalSets: number  
+	  
+	exercises: {  
+    name: string,
+    totalVolume: number,
+    totalSets: number  
+  }[]
+
+	sets: {  
+    weight: number  
+    reps: number
+    order_index: number 
+  }[]
+} 
+
+//types for validation
 export type WorkoutValidationErrors =
   Record<
     string,
