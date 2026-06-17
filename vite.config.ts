a9e47f15e264c065f7eea4815defdad809d135f7
Pavel Prigoda <rotares@disroot.org>
2026-06-17 17:42:27 +0300

    build(dep): update vitest and ts configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { URL, fileURLToPath } from "url"
-import { defineConfig } from "vite"
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { visualizer } from 'rollup-plugin-visualizer';
+import { URL, fileURLToPath } from "url";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), visualizer({ open: true, filename: 'bundle-analysis.html' })],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
